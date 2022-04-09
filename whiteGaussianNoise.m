9835d3e771ab5b6a9fa911a42b0109e2894110f9
soumya <soumya.d.mohanty@gmail.com>
2022-04-09 12:07:56 -0500

    SDM changes and comments.

--- whiteGaussianNoise.m
+++ whiteGaussianNoise.m
@@ -1,9 +1,13 @@
 %% Generation of White Gaussian Noise
+%FIXME Doc: A better title would be "Noise whitening". You are not generating a new noise realization, just whitening a given one.
 
 % Load the data file
 data = load('testData.txt');
 % Signal parameters
+%FIXME Doc: What does the comment "Signal parameters" mean here?
+
 % Sampling frequency for noise realization
+%FIXME Use the name of the unit 'Hz' instead of the name of the scientist, 'Hertz'.
 sampFreq = 1024; %Hertz
 % Number of samples in the data file
 nSamples = 16384;
@@ -17,14 +21,17 @@ inSigFree = data(data(:,1) <= 5, 2);
 figure;
 plot(freqVec,psdVals);
 title('Estimated Power Spectral density');
+%FIXME Plot: Use 'Hz' instead of 'Hertz'
 xlabel('Frequency (Hertz)');
 ylabel('Power spectral density');
 
 %% Generate White Gaussian noise realization
+%FIXME Doc: You are not generating a realization, whitening an existing one: Change the section title
 inNoise = data(:,2);
 % Design FIR filter with T(f)= 1/square root of estimated Power Spectral
 % Density
 % Estimate Power Spectral Density
+%FIXME VarName: 'sqrtPSD' is not an appropriate name for the reciprocal of sqrt(psdVals)
 sqrtPSD = 1./sqrt(psdVals);
 fltrOrdr = 500;
 b = fir2(fltrOrdr,freqVec/(sampFreq/2),sqrtPSD);
@@ -33,6 +40,7 @@ outNoise = sqrt(sampFreq)*fftfilt(b,inNoise);
 % Plot the Colored Gaussian Noise Realization
 figure;
 plot(timeVec, inNoise);
+%FIXME Plot: Title is not indicative of plot (use 'Data' instead)
 title('Colored Noise');
 xlabel('sampling time(sec)');
 ylabel('data value');
@@ -40,6 +48,7 @@ ylabel('data value');
 % Plot the White Gaussian noise realization
 figure;
 plot(timeVec, outNoise);
+%FIXME Plot: Title is not indicative of plot (use 'Whitened Data' instead)
 title('White Noise');
 xlabel('sampling time(sec)');
 ylabel('data value');
